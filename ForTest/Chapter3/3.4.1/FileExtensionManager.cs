43d1dfc5e44799c18e6165358de6686bf2145ef7
zhixin9001 <zhixin9001@126.com>
2017-05-22 07:43:15 +0800

    3.4 Repeat

---
+++ ForTest/Chapter3/3.4.1/FileExtensionManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn.Chapter3._3._4._1
+{
+  public class FileExtensionManager : IExtensionManager
+  {
+    public bool IsValid(string fileName)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ ForTest/Chapter3/3.4.1/IExtensionManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn.Chapter3._3._4._1
+{
+  public interface IExtensionManager
+  {
+    bool IsValid(string fileName);
+  }
+}
